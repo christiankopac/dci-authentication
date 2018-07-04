b0a1fe78dc5bbd5058a5e5be403e09f71e33439b
Christian Kopac <christian.kopac@gmail.com>
2018-07-04 11:42:23 +0200

    Rewrote login app

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://dciauth:dciauthenticationcatsforever99@ds227171.mlab.com:27171/dci-authentication'
+};
+
+
+
--- db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'url': 'mongodb://<dbuser>:<dbpassword>@ds227171.mlab.com:27171/dci-authentication'
-}
